d79592f8450b80140268bb66c62b3a0a3251da8f
Guilherme1991 <gui23091991@gmail.com>
2020-09-07 00:21:59 -0300

    Protótipo Brasmodulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'quem-somos', component: QuemSomosComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'contato', component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    QuemSomosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
